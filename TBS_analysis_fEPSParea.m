b0d1113b41a73f4b9ac6718300b17c9d2166b27a
gregkronberg <gregkronberg@gmail.com>
2017-04-04 14:57:42 -0400

    Preallocated matrices, cleaned up comments

--- TBS_analysis_fEPSParea.m
+++ TBS_analysis_fEPSParea.m
@@ -6,8 +6,8 @@ fpath = 'D:\Google Drive\Work\Research Projects\Theta LTP\Processed Matlab Data\
 direct = dir(strcat(fpath,'*.mat*')); % processed matlab files
 
 %% define conditions
-induction = {'_TBS'}; % plasticity induction protocol
-stim = {'_control';'_cathodal';'_anodal'};% DCS stimulation conditions
+induction = {'_TBS'};                                                       % plasticity induction protocol
+stim = {'_control';'_cathodal';'_anodal'};                                  % DCS stimulation conditions
 intensity = [0,5,20]; % stimulation intensity in V/m
 position = {'_apical';'_basal';'_perforant'}; % recording location in slice
 locs = {'A','B','P','S'}; % loc 
@@ -57,6 +57,10 @@ dfiltHigh2 = designfilt('bandpassiir','FilterOrder',20,...
 
 %% arrange file names by condition
 slices = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+slice = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+cutoff(apical) = 20170115;
+cutoff(basal) = 20170301;
+cutoff(perforant) = 0;
 for a = 1:length(induction)
     for b = 1:length(stim)
         for c = 1:length(intensity)
@@ -67,8 +71,8 @@ for a = 1:length(induction)
                            position{d},drug{e},'*.mat'));
                        % choose slices after a certain date
                        g = 0;
-                       for f = 1:length(slice{a,b,c,d,e});
-                           if 20161024<str2double(slice{a,b,c,d,e}(f).name(1:8));
+                       for f = 1:length(slice{a,b,c,d,e})
+                           if cutoff(d)<str2double(slice{a,b,c,d,e}(f).name(1:8))
                                g = g+1;
                                slices{a,b,c,d,e}(g) = slice{a,b,c,d,e}(f);
                            end
@@ -82,6 +86,16 @@ end
 %% load each data file and store processed data
 baseArea = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
 indFilt1 = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+indAll = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+slopesN = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+slopes = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+slopesEnd = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+heights = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+dates = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+hemis = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+burstArea = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+burstAreaN = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
+burstAreaNmean = cell(length(induction),length(stim),length(intensity),length(position),length(drug));
 X = [];
 for d = 1:length(position)
     figure(d);hold on
@@ -98,34 +112,36 @@ for d = 1:length(position)
                                 'indBlock','spike','spikeN',...
                                 'height','hemi','age','current','date','indS4',...
                                 'indS','fs','baseS','pulset','indD','indD4','baseD','slopesD');
+                            
+                            % check that there are enough recording blocks
                             if length(slopesD)>=indBlock(1)+tpost-1
-                                slopes{a,b,c,d,e}(:,f) = slopesD([indBlock(1)-tbase:indBlock(1)-1,indBlock(1):indBlock(1)+tpost-1]);
+                                slopes{a,b,c,d,e}(:,f) = slopesD([indBlock(1)-tbase:indBlock(1)-1,indBlock(1):indBlock(1)+tpost-1]); % slopes x slices
                             end
-                            indAll{a,b,c,d,e}(:,f) = indD;
-                            base = baseD((pulset+bipolarWidth):pulset+(fs/rBurst),:);
-                            baseAll = baseD;
+                            indAll{a,b,c,d,e}(:,f) = indD;  % induction traces (time x slices)
+                            base = baseD((pulset+bipolarWidth):pulset+(fs/rBurst),:); % cropped baseline traces (time x blocks)
+                            baseAll = baseD; % full baseline traces (time x blocks)
                             
                             % slopes and heights
                             % normalizes slopes
                             slopesN{a,b,c,d,e}(:,f) = slopes{a,b,c,d,e}(:,f)/...
-                            mean(slopes{a,b,c,d,e}(1:tbase,f));
+                                mean(slopes{a,b,c,d,e}(1:tbase,f)); % (blocks x slices)
                         
                             % slope after 60 min for each slice
-                            slopesEnd{a,b,c,d,e}(f) = mean(slopesN{a,b,c,d,e}(end-9:end,f));
+                            slopesEnd{a,b,c,d,e}(f) = mean(slopesN{a,b,c,d,e}(end-9:end,f)); % (slices x 1)
                             
                             % height, date, hemisphere
-                            heights{a,b,c,d,e}(f) = height;
-                            dates{a,b,c,d,e}(f) = date;
-                            hemis{a,b,c,d,e}(f) = hemi;
+                            heights{a,b,c,d,e}(f) = height;% (slices x 1)
+                            dates{a,b,c,d,e}(f) = date;% (slices x 1)
+                            hemis{a,b,c,d,e}(f) = hemi;% (slices x 1)
                             
                             % area under baseline fEPSPs
                             % Normalize first sample to zero
-                            base1 = ones(size(base,1),1)*base(1,:);
-                            base = base-base1;
+                            base1 = ones(size(base,1),1)*base(1,:); % each column is the value of the first sample for that block
+                            base = base-base1; % normalized and cropped baseline trace (time x blocks)
                             
                             % Area under fEPSP
                             baseArea{a,b,c,d,e}(f) = mean(sum(abs(base(:,...
-                                indBlock(1)-20:indBlock(1)-1)),1));
+                                indBlock(1)-20:indBlock(1)-1)),1)); % average area under basleine fEPSP
                             
                             
                             % filter time series recordings during induction to remove drift
@@ -136,22 +152,22 @@ for d = 1:length(position)
                         end
                         % Make each column the time series of a single burst
                         % 3rd dimension is slice number
-                        indFilt1{a,b,c,d,e} = indFilt1{a,b,c,d,e}(tbsOn+1:tbsOff,:);
+                        indFilt1{a,b,c,d,e} = indFilt1{a,b,c,d,e}(tbsOn+1:tbsOff,:); % traces during TBS (time x slices)
                         indFilt1{a,b,c,d,e} = reshape(indFilt1{a,b,c,d,e},[],...
-                            nBurst,length(slices{a,b,c,d,e}));
+                            nBurst,length(slices{a,b,c,d,e})); % reshape (time x burst x slice)
                         
                         % remove time between bursts
-                        indFilt1{a,b,c,d,e} = indFilt1{a,b,c,d,e}(1:tBurst,:,:);
+                        indFilt1{a,b,c,d,e} = indFilt1{a,b,c,d,e}(1:tBurst,:,:); % (time x burst x slice)
                         
                         % remove bipolar pulse
-                        indFilt1{a,b,c,d,e}(bipolarT,:,:) = [];
+                        indFilt1{a,b,c,d,e}(bipolarT,:,:) = []; % (time x burst x slice)
                         
                         % Normalize first sample to zero
                         indFilt1{a,b,c,d,e} = indFilt1{a,b,c,d,e}-...
-                            indFilt1{a,b,c,d,e}(1,:,:);
+                            indFilt1{a,b,c,d,e}(1,:,:); %(time x burst x slice)
                         
-                        % integrate under the each burst envelope
-                        burstArea{a,b,c,d,e} = permute(sum(abs(indFilt1{a,b,c,d,e}),1),[2,3,1]);
+                        % integrate under each burst envelope
+                        burstArea{a,b,c,d,e} = permute(sum(abs(indFilt1{a,b,c,d,e}),1),[2,3,1]); % (burst x slices)
                         
                         % Normalize to area during baseline
                         burstAreaN{a,b,c,d,e} = burstArea{a,b,c,d,e}./...
@@ -160,26 +176,12 @@ for d = 1:length(position)
                         % Average burst area
                         burstAreaNmean{a,b,c,d,e} = mean(burstAreaN{a,b,c,d,e},1);
                         
-                        %% regression
-                        % load data into a single matrix for regression
-                        X = [X;d*ones(length(slopesEnd{a,b,c,d,e}),1),...
-                                b*ones(length(slopesEnd{a,b,c,d,e}),1),...
-                                burstAreaNmean{a,b,c,d,e}',heights{a,b,c,d,e}',...
-                                slopesEnd{a,b,c,d,e}'];
-                        
-                        indices = X(:,1)==d & X(:,2)==b;
-                        if sum(indices)>size(X,2)-3;
-                            reg(b,d) = regstats(X(indices,end),X(indices,3:end-1));
-                        end
-                        
-                        date_cut = 20170115;
-                        date_keep = dates{a,b,c,d,e}>date_cut;
                         %% figures
-                            figure(d)
-                            plot(burstAreaNmean{a,b,c,d,e}(date_keep),slopesEnd{a,b,c,d,e}(date_keep),'.','Color',stimcolor{b},'MarkerSize',15)
-                            xlabel('Normalized area under theta burst dendritic recording')
-                            ylabel('Synaptic strength')
-                            title(position{d}(2:end))
+                        figure(d)
+                        plot(burstAreaNmean{a,b,c,d,e},slopesEnd{a,b,c,d,e},'.','Color',stimcolor{b},'MarkerSize',15)
+                        xlabel('Normalized area under theta burst dendritic recording')
+                        ylabel('Synaptic strength')
+                        title(position{d}(2:end))
                     end
                 end
             end
