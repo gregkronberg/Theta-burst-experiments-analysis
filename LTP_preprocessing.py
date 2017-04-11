f4e3dce8a68449a8f89ee7e41ea6d2130e6613e5
gregkronberg <gregkronberg@gmail.com>
2017-04-11 14:07:22 -0400

    Slopes of baseline traces

--- LTP_preprocessing.py
+++ LTP_preprocessing.py
@@ -21,6 +21,10 @@ from scipy import stats
 import scipy.io as sio
 import os
 import re 
+import pylab
+
+# close figures
+plt.close('all')
 
 #==============================================================================
 # Set up directories
@@ -112,23 +116,18 @@ for slice in dir_new:
     
     #==============================================================================
     # organize raw data into arrays
-    #==============================================================================
-    # bipolar pulse paramters
-    pulse_date = 20161013 # changed timing of bipolar pulse after this date
-    if date>= pulse_date:
-        pulse_t = 0.5*fs
-    else:
-        pulse_t = 0
-        
+    #==============================================================================  
     # baseline index
-    base_idx_pre = np.arange(ind_block[0]-baset_pre,ind_block[0])
-    base_idx_post = np.arange(ind_block[0]+1,ind_block[0]+baset_post+1)
+    base_idx_pre = np.arange(ind_block[0]-baset_pre-1,ind_block[0]-1)
+    base_idx_post = np.arange(ind_block[0],ind_block[0]+baset_post)
     base_idx = np.concatenate((base_idx_pre,base_idx_post),0)
     
     # organize baseline traces into column vectors
     # preallocate
-    base_soma = np.empty([int(stats.mode(l_block,None).mode),matfile['datastart'].shape[1]])
-    base_dend = np.empty([int(stats.mode(l_block,None).mode),matfile['datastart'].shape[1]])
+    base_soma = np.empty([int(stats.mode(l_block,None).mode),
+                          matfile['datastart'].shape[1]])
+    base_dend = np.empty([int(stats.mode(l_block,None).mode),
+                          matfile['datastart'].shape[1]])
     
     # loop over blocks
     for a in np.arange(0,matfile['datastart'].shape[1],dtype=int): 
@@ -136,15 +135,18 @@ for slice in dir_new:
         # check that it is a baseline block (not induction)
         if l_block[1,a] == stats.mode(l_block,None).mode: 
             
-            # check for somatic recording 
+            # loop over possible recording locations
             for idx,loc in enumerate(rec_loc):
+                # check for somatic recording
                 if loc == 'Soma':
                     if loc_chan[idx] != -1:
                         # indeces for each block 
-                        soma_idx = np.arange(matfile['datastart'][int(loc_chan[idx]),a],
-                                     matfile['dataend'][int(loc_chan[idx]),a],dtype=int)
+                        soma_idx = np.arange(matfile['datastart']
+                            [int(loc_chan[idx]),a],matfile['dataend']
+                            [int(loc_chan[idx]),a],dtype=int)
                         # store each block as a column
                         base_soma[:,a] = matfile['data'][:,soma_idx].T.reshape(-1)
+                # repeat for dendritic recording
                 elif loc_chan[idx] != -1:
                     dend_idx = np.arange(matfile['datastart'][loc_chan[idx],a],
                                      matfile['dataend'][loc_chan[idx],a],dtype=int)
@@ -155,12 +157,45 @@ for slice in dir_new:
     if base_dend.shape[1]<max(base_idx):
         # repeat last block to fill gap
         base_dend = np.concatenate((base_dend,base_dend[:,-1].reshape(-1,1)),axis=1)
+        base_idx = np.concatenate(base_idx,base_idx[-1]+1)
     if base_soma.shape[1]<max(base_idx):
         base_soma = np.concatenate((base_soma,base_soma[:,-1].reshape(-1,1)),axis=1)
         
     #==============================================================================
     # take slopes of baseline traces     
     #==============================================================================
+    # bipolar pulse paramters
+    pulse_date = 20161013 # changed timing of bipolar pulse after this date
+    if date>= pulse_date:
+        pulse_t = 0.5*fs
+    else:
+        pulse_t = 0
+        
+    # Time window to plot traces and select slopes
+    slope_t1 = int(pulse_t + .001*fs)
+    slope_t2 = int(pulse_t + .01*fs)
+    
+    # plot traces
+    slope_plot = plt.figure()
+    plt.plot(base_dend - base_dend[slope_t1,:])
+    pylab.xlim([slope_t1,slope_t2])
+    
+    # select time window to take slope
+    slope_win = plt.ginput(2)
+    slope_win1 = int(slope_win[0][0])
+    slope_win2 = int(slope_win[1][0])
+    
+    # take raw slope
+    slopes_raw = np.mean(np.diff(base_dend[np.arange(slope_win1,slope_win2),:],
+                                           axis=0),axis=0)*fs
+    
+    # normalize slopes
+    slopes_base_mean = np.mean(slopes_raw[base_idx[0:baset_pre]])
+    
+    slopes_norm = slopes_raw/slopes_base_mean
+    
+    
+    
         
         
         
