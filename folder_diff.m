a2b1334043c639578cdfbcbec2f7275579e753c5
gregkronberg <gregkronberg@gmail.com>
2017-05-03 21:00:21 -0400

    New Function
    Checks for new files in a folder

--- folder_diff.m
+++ folder_diff.m
@@ -1,8 +1,15 @@
-function new = folder_diff(directory1,directory2,file_type)
+function [new,direct1,direct2] = folder_diff(directory1,directory2,file_type)
+
+% new = cell containing a list of different files between the two
+% directories
+
+% direct1 = cell containing list of all files in directory 1
+
+% direct2 = cell containing list of all files in directory 2
 
 % this function compares two directories and returns a cell containing file
 % names of files that are different between the directories.  Note that the
-% search is direction, i.e.files in directory1 are searched for in
+% search is directional, i.e.files in directory1 are searched for in
 % directory2.  Any unique files in directory2 are not identified.
 
 % list contents of each directory as a structure
@@ -20,10 +27,13 @@ for a = 1:length(direct2_struct)
 end
 
 cnt = 0 ;
-new = cell(0)
+new = cell(0);
+% loop over files in first directory
 for a = 1:length(direct1)
-    if sum(strcmp(direct2,direct1{a}))==0                              % check if current slice is new
+    % check if file exists in second directory
+    if sum(strcmp(direct2,direct1{a}))==0                         
         cnt = cnt+1;
+        % if file is unique, store its name
         new{cnt} = direct1{a};
     end
 end
